8e6d104d80a30e4ac444517221c0c7f050092f80
songcw8 <ben990158@gmail.com>
2025-04-22 15:18:42 +0900

    Initial commit

---
+++ src/test/java/org/example/bootjwt/BootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bootjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
